67258364eeb50249b212cc4e9ae815ebeee1c40c
vitas2048 <vithag97@mail.ru>
2022-12-21 15:46:34 +0700

    3. Состояние нити. [#229175 #372605]

--- src/main/java/ru/job4j/concurrent/ThreadState.java
+++ src/main/java/ru/job4j/concurrent/ThreadState.java
@@ -10,7 +10,9 @@ public class ThreadState {
         );
         first.start();
         second.start();
-        while (first.getState() != Thread.State.TERMINATED && second.getState() != Thread.State.TERMINATED) {
+        while (first.getState() != Thread.State.TERMINATED || second.getState() != Thread.State.TERMINATED) {
+            System.out.println("first thread status: " + first.getState());
+            System.out.println("second thread status: " + second.getState());
         }
         System.out.println("Threads are Terminated");
     }
