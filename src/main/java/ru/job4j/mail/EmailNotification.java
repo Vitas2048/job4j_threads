4faf9ab695c46496c81a65762189c6ffa02e386a
vitas2048 <vithag97@mail.ru>
2023-01-10 15:28:45 +0700

    2. ExecutorService рассылка почты. [#63097 #379096]

--- src/main/java/ru/job4j/mail/EmailNotification.java
+++ src/main/java/ru/job4j/mail/EmailNotification.java
@@ -4,23 +4,27 @@ import java.util.concurrent.Executor;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
-public class EmailNotification {
+public final class EmailNotification {
 
-    ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
+    private final ExecutorService pool = Executors.newFixedThreadPool(Runtime.getRuntime().availableProcessors());
 
     public void emailTo(User user) {
-        pool.submit(new Runnable() {
-            @Override
-            public void run() {
-                String subject = String.format("Notification %s to email %s", user.getUserName(), user.getEmail());
-                String body = String.format("Add a new event to %s", user.getUserName());
-                send(subject, body, user.getEmail());
-            }
+        pool.submit(() -> {
+            String subject = String.format("Notification %s to email %s", user.getUserName(), user.getEmail());
+            String body = String.format("Add a new event to %s", user.getUserName());
+            send(subject, body, user.getEmail());
         });
     }
 
     public void close() {
         pool.shutdown();
+        while (!pool.isTerminated()) {
+            try {
+                Thread.sleep(100);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
     public void send(String subject, String body, String email) {
